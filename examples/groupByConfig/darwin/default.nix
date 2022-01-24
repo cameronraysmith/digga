6cd623e0be63f77c8653612e65b214b8691ab028
Chris Montgomery <chris@alley.co>
2022-01-24 00:48:35 -0500

    test!: try disabling darwin host tests?

--- examples/groupByConfig/darwin/default.nix
+++ examples/groupByConfig/darwin/default.nix
@@ -6,7 +6,8 @@ in
   hostDefaults.channelName = "nixos";
   hosts = {
     Darwinia.modules = [ ./Darwinia.nix ];
-    Darwinia.tests = [ allProfilesTest ];
+    # Darwinia.tests = [ allProfilesTest ];
+    Darwinia.tests = [ ];
   };
   importables = rec {
     suites = rec {
